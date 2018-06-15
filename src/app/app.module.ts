a44a030a13db5fed70a086f4fc97476c94121aa9
Sebastian Krome <sebastian.krome@cgi.com>
2018-06-15 20:40:34 +0200

    GoogleService entfernt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { GoogleService } from './services/google/google.service';
 import { HttpModule } from '@angular/http';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
@@ -23,7 +22,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     environment.production ? ServiceWorkerModule.register('ngsw-worker.js') : []
   ],
   exports : [],
-  providers: [GoogleService, PlaceMarkerService],
+  providers: [PlaceMarkerService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
