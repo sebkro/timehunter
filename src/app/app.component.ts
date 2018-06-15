ccc3630774720cbe0f91dfb0726fc20495781a00
Sebastian Krome <sebastian.krome@cgi.com>
2018-06-15 22:23:02 +0200

    Update aktuelle Position auf Map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,12 +26,14 @@ import {
 })
 export class AppComponent implements OnInit {
   title = 'Raw';
+  map: google.maps.Map;
+  positionMarker: google.maps.Marker;
+
+  @ViewChild('gmap') gmapElement: any;
+
 
   constructor(private markerService: PlaceMarkerService,
     private locationService: LocationService) { }
-
-  @ViewChild('gmap') gmapElement: any;
-  map: google.maps.Map;
 
   ngOnInit() {
     const mapProp = {
@@ -42,14 +44,24 @@ export class AppComponent implements OnInit {
     this.locationService.findStartingPoint(53.5422562, 9.9891803).subscribe(res => alert(JSON.stringify(res)));
     this.map = new google.maps.Map(this.gmapElement.nativeElement, mapProp);
     if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        success => {
-          this.map.setCenter(new google.maps.LatLng(success.coords.latitude, success.coords.longitude));
-          this.markerService.setPositionMarker(this.map, success.coords.latitude, success.coords.longitude);
-        }, error => {
-          alert('bla');
-        });
+      this.updatePosition(this.map);
     }
+  }
+
+  private updatePosition(map: google.maps.Map) {
+    navigator.geolocation.getCurrentPosition(
+      success => {
+        if (this.positionMarker) {
+          this.positionMarker.setPosition(new google.maps.LatLng(success.coords.latitude, success.coords.longitude));
+        } else {
+          map.setCenter(new google.maps.LatLng(success.coords.latitude, success.coords.longitude));
+          this.positionMarker = this.markerService.setPositionMarker(this.map, success.coords.latitude, success.coords.longitude);
+        }
+        console.log('new Position: ' + success.coords.latitude + ' ' + success.coords.longitude);
+        setTimeout(() => this.updatePosition(map), 5000);
+      }, error => {
+        alert('bla');
+      });
 
   }
 
